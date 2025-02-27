86695f34c7772a9bf5f3e77c5ec8a213564b13bb
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-02-27 15:10:04 +0530

    Weather Struture

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="main">
+      <div className="side_box">
+        <h4>current weather</h4>
+        <p>2:37 PM</p>
+        <div className="weather-info">
+          <img src="/sun.png" alt="Weather Icon" />
+          <h2>30°C</h2>
+        </div>
+      </div>
+
+      
     </div>
+
+    <div className="Additional">
+      <div className="a1">
+        <h4>Air Quality</h4>
+        <p>113</p>
+        
+      </div>
+      <div className="w1">
+        <h4> Wind</h4>
+        <p>4 km/h</p>
+      </div>
+      <div className="h1">
+        <h4> Humidity</h4>
+        <p>22 %</p>
+      </div>
+      <div className="v1">
+        <h4>Visibility</h4>
+        <p>10 km</p>
+      </div>
+      <div className="p1">
+        <h4> Pressure</h4>
+        <p>1015 mb</p>
+      </div>
+      <div className="d1">
+        <h4> Dew point</h4>
+        <p>8*</p>
+      </div>
+    </div>
+    </>
+    
   );
-}
+};
 
 export default App;
